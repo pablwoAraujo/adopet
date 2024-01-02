13061fe15627bf0d186745c7ddd6af48e9613fac
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-01-02 15:15:59 -0300

    test: verificando o comportamento quando existem abrigos cadastrados

--- adopet/src/main/java/br/com/alura/domain/Shelter.java
+++ adopet/src/main/java/br/com/alura/domain/Shelter.java
@@ -21,20 +21,47 @@ public class Shelter {
 		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getNome() {
 		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getTelefone() {
 		return telefone;
 	}
 
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 	public String getEmail() {
 		return email;
 	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public Pet[] getPets() {
 		return pets;
+	}
+
+	public void setPets(Pet[] pets) {
+		this.pets = pets;
+	}
+
+	@Override
+	public String toString() {
+		return """
+				"id":%s,"nome":"%s","telefone":"%s","email":"%s"
+				""".formatted(this.id, this.nome, this.telefone, this.email);
 	}
 
 }
---
+++ adopet/src/test/java/br/com/alura/service/ShelterServiceTest.java
@@ -0,0 +1,46 @@
+package br.com.alura.service;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.domain.Shelter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.http.HttpResponse;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ShelterServiceTest {
+
+	private ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
+	private ShelterService abrigoService = new ShelterService(client);
+	private HttpResponse<String> response = mock(HttpResponse.class);
+	private Shelter abrigo = new Shelter("Teste", "61981880392", "abrigo_alura@gmail.com");
+
+	@Test
+	public void shouldCheckWhenThereHasShelter() throws IOException, InterruptedException {
+		abrigo.setId(0L);
+		String expectedAbrigosCadastrados = "Abrigos cadastrados:";
+		String expectedIdENome = "0 - Teste";
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(baos);
+		System.setOut(printStream);
+
+		when(response.body()).thenReturn("[{" + abrigo.toString() + "}]");
+		when(client.triggerGetRequest(anyString())).thenReturn(response);
+
+		abrigoService.listShelters();
+
+		String[] lines = baos.toString().split(System.lineSeparator());
+		String actualAbrigosCadastrados = lines[0];
+		String actualIdENome = lines[1];
+
+		Assertions.assertEquals(expectedAbrigosCadastrados, actualAbrigosCadastrados);
+		Assertions.assertEquals(expectedIdENome, actualIdENome);
+	}
+}
